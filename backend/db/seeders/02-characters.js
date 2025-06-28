b967ce00a6990000958bf65cdc70ca4e9a7d5150
Rashid <rjnormal757@gmail.com>
2025-06-27 22:45:31 -0400

    Deployment Fix

--- backend/db/seeders/02-characters.js
+++ backend/db/seeders/02-characters.js
@@ -33,7 +33,7 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete(options, {
+    await queryInterface.bulkDelete('Characters', {
       name: { [Sequelize.Op.in]: ['Kaelen Duskthorn', 'Brakka Stonegut'] },
     });
   },
